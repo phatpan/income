f62ac786456118473e03f1d3ce6eecefe21f1dd7
phatcharaphan <phat.pan@gmail.com>
2017-05-20 18:00:04 +0700

    create connection mongodb and insert data

--- api/src/main.go
+++ api/src/main.go
@@ -1,6 +1,9 @@
 package main
 
-import mgo "gopkg.in/mgo.v2"
+import (
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
 
 var (
 	Mongo_session    *mgo.Session
@@ -16,5 +19,17 @@ const (
 )
 
 func main() {
+	mongo_session, err := mgo.Dial(DATABASE_SERVER)
+	if err != nil {
+		panic(err)
+	}
 
+	mongo_session.SetMode(mgo.Monotonic, true)
+	Mongo_session = mongo_session
+	Users_collection = mongo_session.DB(DATABASE_NAME).C(DATABASE_COLLECTION_EXPENSE)
+	defer Mongo_session.Close()
+
+	err = Users_collection.Insert(bson.M{
+		"amount": "1000",
+	})
 }
