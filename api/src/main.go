ebc58256ab2fb7db8755f17d2d59005ea150efa3
phatcharaphan <phat.pan@gmail.com>
2017-05-20 18:27:10 +0700

    refactor insert to mongodb

--- api/src/main.go
+++ api/src/main.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	mgo "gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
-)
+import mgo "gopkg.in/mgo.v2"
 
 var (
 	Mongo_session    *mgo.Session
@@ -18,6 +15,11 @@ const (
 	DATABASE_COLLECTION_REVENUE = "revenue"
 )
 
+type Expense struct {
+	Amount      string `json:"amount"`
+	Description string `json:"description"`
+}
+
 func main() {
 	mongo_session, err := mgo.Dial(DATABASE_SERVER)
 	if err != nil {
@@ -29,7 +31,17 @@ func main() {
 	Users_collection = mongo_session.DB(DATABASE_NAME).C(DATABASE_COLLECTION_EXPENSE)
 	defer Mongo_session.Close()
 
-	err = Users_collection.Insert(bson.M{
-		"amount": "1000",
-	})
+	expense := Expense{
+		Amount:      "500",
+		Description: "ค่าเดินทาง",
+	}
+	expense.SaveToDB()
+}
+
+func (e *Expense) SaveToDB() error {
+	err := Users_collection.Insert(&e)
+	if err != nil {
+		return err
+	}
+	return nil
 }
